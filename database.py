b1868c2d281fa87b2c256eea18e993e21ad73584
Faices <22faices@gmail.com>
2024-04-02 13:47:29 +0200

    insert one instead of update in mongodb stats

--- database.py
+++ database.py
@@ -13,5 +13,5 @@ def store_post_data(post_data, topic):
 def store_subreddit_stats(stats, subreddit_name):
     """Store subreddit statistics in a MongoDB collection."""
     collection = db['subreddit_stats']
-    # You might want to update existing stats instead of inserting new ones each time
-    collection.update_one({'name': subreddit_name}, {'$set': stats}, upsert=True)
+    # Insert new document instead of updating
+    collection.insert_one(stats)
